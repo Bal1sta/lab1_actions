a3a6785ad74dbc3e009805a95fc384cb41112d33
Ruslan <bali1sta.0816@gmail.com>
2024-09-21 16:44:19 +0700

    14

---
+++ out/build/GCC 13.2.0 x86_64-w64-mingw32 (mingw64)/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/lAbA/lab1_actions
+# Build directory: C:/lAbA/lab1_actions/out/build/GCC 13.2.0 x86_64-w64-mingw32 (mingw64)
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_version "test_version")
+set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "C:/lAbA/lab1_actions/CMakeLists.txt;70;add_test;C:/lAbA/lab1_actions/CMakeLists.txt;0;")
